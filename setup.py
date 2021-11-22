6448bda12a33ec8bd7e5762693e49584f80efdf9
kzhou57 <kevin.zhou@gmi-mr.com>
2021-11-22 11:03:03 -0800

    New version for yaml safe operations

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = open('README.rst' if os.path.exists('README.rst') else 'READM
 
 setup(
     name='confluence-publisher',
-    version='1.2.1',
+    version='1.2.1.1',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     license='MIT',
